517e709fbbb4bb066b08978a3c3326bf6b5b8e25
Rabab Sh <36003555+RababMojahed@users.noreply.github.com>
2019-01-22 19:07:59 +0300

    updated footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -13,7 +13,7 @@
 			</ul>	
 		</div>		
 		<em>&copy;all Copyrights Reserved for Ayadaty 2019</em>
-		<a href="mailto:rabab@ihsanit.com">Developed by: Rabab Shalan</a>
+		<a href="mailto:talent@ihsanit.com">Developed by: Ihsan IT</a>
 		</footer>
 	</body>
 </html>
