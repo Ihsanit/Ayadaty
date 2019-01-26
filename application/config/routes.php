f3712560801cae5b1eec2e0b96ca3ccd93eca0e4
Mohammed Abdulaziz Al-Tamimi <mohammed@ihsanit.com>
2019-01-26 13:46:31 +0300

    minimize url for login and signup with setting the proper

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,56 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+//A URL with "signup" in the first segment will be remapped to the "user_c" controller and signup method.
+$route['signup']='user_c/signup';
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	https://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
-*/
-$route['user/signup']='user_c/signup';//URI specified
-$route['user/login']='user_c/login';
+//A URL with "login" in the first segment will be remapped to the "user_c" controller and login method.
+$route['login']='user_c/login';
 $route['default_controller'] = 'home_c';
 
 $route['(:any)']='home_c';//URI specified
