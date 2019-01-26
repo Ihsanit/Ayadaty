a524b0b4914f0237a19039780383b3c85dce7329
Mohammed Abdulaziz Al-Tamimi <mohammed@ihsanit.com>
2019-01-26 13:57:26 +0300

    adding route rule for booking page, some enhancement for anchor elements in navigation bar

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,6 +5,10 @@ $route['signup']='user_c/signup';
 
 //A URL with "login" in the first segment will be remapped to the "user_c" controller and login method.
 $route['login']='user_c/login';
+
+//A URL with "booking" in the first segment will be remapped to the "patient_c" controller and book_appointment method.
+$route['booking'] = 'patient_c/book_appointment';
+
 $route['default_controller'] = 'home_c';
 
 $route['(:any)']='home_c';//URI specified
--- application/views/template/navigation.php
+++ application/views/template/navigation.php
@@ -1,14 +1,14 @@
 		<nav>
 			<div class="login-menu">			
 				<a onclick="document.getElementById('id01').style.display='block'" style="cursor:pointer;"><i class="fas fa-sign-in-alt"></i> Login</a>
-				<a href="<?php echo base_url();?>user_c/signup"><i class="fas fa-user-plus"></i> Sign Up</a>
+				<a href=<?php echo base_url('signup');?>><i class="fas fa-user-plus"></i> Sign Up</a>
 				<a href="#">Log out</a>
 			</div> 
 			<label for="drop" class="toggle">Menu</label>
 			<input type="checkbox" id="drop"/>
 			<ul class="nav menu">
 				<li>
-			        <a href="<?php echo base_url();?>home_c/index" class="active"><i class="fa fa-fw fa-home"></i>Home</a>
+			        <a href=<?php echo base_url();?> class="active"><i class="fa fa-fw fa-home"></i>Home</a>
 			    </li>
 			    <li>
 			        <a href="#">About</a>
@@ -48,7 +48,7 @@
 			    </li>
 			    <li style="margin-left:380px">
 			    
-			    	<a href="<?php echo base_url();?>patient_c/book_appointment" class="btn " >Book Appointment</a>
+			    	<a href=<?php echo base_url('booking');?> class="btn " >Book Appointment</a>
 			    	
 			    </li>
 			    
